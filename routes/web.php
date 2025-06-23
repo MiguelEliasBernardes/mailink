cf733b2b468d13fcf2c68c8c3bbd60b3d5c94706
Miguel Elias <miguelelias20050806@gmail.com>
2025-06-23 00:27:08 -0300

    :tada: Commit inicial

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Http\Controllers\EmailListController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/lists', [EmailListController::class, 'index'])->name('email-list.index');
+    Route::post('/lists'. [EmailListController::class. 'create'])->name('email-list.index');
+
+});
+
+require __DIR__.'/auth.php';
